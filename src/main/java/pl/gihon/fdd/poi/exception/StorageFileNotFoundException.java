142131e89b827b0367c6ef0f072e411daf52cca3
pishon <szybkistart24@gmail.com>
2016-09-25 13:11:20 +0200

    actually stores in tmp folder, reads from tmp folder

--- src/main/java/pl/gihon/fdd/poi/exception/StorageFileNotFoundException.java
+++ src/main/java/pl/gihon/fdd/poi/exception/StorageFileNotFoundException.java
@@ -2,6 +2,10 @@ package pl.gihon.fdd.poi.exception;
 
 public class StorageFileNotFoundException extends RuntimeException {
 
+	public StorageFileNotFoundException(String filename) {
+		throw new RuntimeException("file " + filename + " not found on storage");
+	}
+
 	/**
 	 * 
 	 */
--- src/main/java/pl/gihon/fdd/poi/io/StorageServiceTmpFolder.java
+++ src/main/java/pl/gihon/fdd/poi/io/StorageServiceTmpFolder.java
@@ -1,22 +1,25 @@
 package pl.gihon.fdd.poi.io;
 
-import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 
-import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import pl.gihon.fdd.poi.exception.StorageFileNotFoundException;
 import pl.gihon.fdd.poi.exception.StorageInternalException;
 
 @Component
 public class StorageServiceTmpFolder implements StorageService {
 
 	private static Logger logger = LoggerFactory.getLogger(StorageService.class);
+
+	private static String TEMP_FOLDER = System.getProperty("java.io.tmpdir");
 
 	public Object loadAll() {
 		logger.info("loading all");
@@ -26,25 +29,34 @@ public class StorageServiceTmpFolder implements StorageService {
 	@Override
 	public Resource loadAsResource(String filename) {
 		logger.info("loading {}", filename);
-		return null;
+		String tmpFolderWithSeparator = tmpFolderWithSeparator();
+		File file = new File(tmpFolderWithSeparator + filename);
+		if (!file.exists()) {
+			throw new StorageFileNotFoundException(filename);
+		}
+		return new FileSystemResource(file);
 	}
 
 	@Override
 	public int store(MultipartFile file) {
 		try {
-			InputStream is = file.getInputStream();
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			IOUtils.copy(is, baos);
-
-			logger.info("storing file");
-			String content = baos.toString();
-			logger.debug("file content as follows");
-			logger.debug("{}", content);
-			return content.length();
+			logger.debug("storing file {} in temp", file);
+			String tmpFolderWithSeparator = tmpFolderWithSeparator();
+			File destFile = new File(tmpFolderWithSeparator + file.getOriginalFilename());
+			file.transferTo(destFile);
+			return (int) file.getSize();
 		} catch (IOException e) {
 			throw new StorageInternalException(e);
 		}
 
+	}
+
+	private static String tmpFolderWithSeparator() {
+		String tmpFolderWithSeparator = TEMP_FOLDER;
+		if (!StringUtils.endsWithIgnoreCase(TEMP_FOLDER, File.separator)) {
+			tmpFolderWithSeparator = tmpFolderWithSeparator + File.separator;
+		}
+		return tmpFolderWithSeparator;
 	}
 
 }
