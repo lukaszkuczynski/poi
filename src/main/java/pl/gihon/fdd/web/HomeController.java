c60f0b810c31992f972fb161a5a175d38544a45b
pishon <szybkistart24@gmail.com>
2016-09-25 12:25:26 +0200

    thymeleaf responds with view

---
+++ src/main/java/pl/gihon/fdd/web/HomeController.java
@@ -0,0 +1,17 @@
+package pl.gihon.fdd.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public ModelAndView home() {
+		ModelAndView mav = new ModelAndView("home");
+		mav.addObject("name", "luk");
+		return mav;
+	}
+
+}
