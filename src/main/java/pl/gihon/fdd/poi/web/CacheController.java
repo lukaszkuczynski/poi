6d312bcf5898f0d5c852aed59543051fea03cc0f
pishon <szybkistart24@gmail.com>
2016-10-13 18:04:33 +0200

    can import in form

--- src/main/java/pl/gihon/fdd/poi/web/CacheController.java
+++ src/main/java/pl/gihon/fdd/poi/web/CacheController.java
@@ -1,16 +1,42 @@
 package pl.gihon.fdd.poi.web;
 
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
+
+import pl.gihon.fdd.poi.localisator.google.CacheTool;
 
 @Controller
 @RequestMapping("/cache")
 public class CacheController {
 
+	private static final RedirectView CACHE_REDIRECT = new RedirectView("/cache");
+	private static final Logger logger = LoggerFactory.getLogger(CacheController.class);
+
+	@Autowired
+	private CacheTool cacheTool;
+
 	@GetMapping
-	public String cache() {
+	public String cache(Model model) {
 		return "cache";
+	}
+
+	@PostMapping("/upload")
+	public RedirectView upload(MultipartFile file, RedirectAttributes redirectAttributes) throws IOException {
+		logger.debug("uploading file size {}", file.getSize());
+		redirectAttributes.addFlashAttribute("message", "uploaded file size " + file.getSize());
+		cacheTool.importToCache(file.getInputStream());
+		return CACHE_REDIRECT;
 	}
 
 }
