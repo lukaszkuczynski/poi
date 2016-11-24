b48713ca20af1ca00af3b5cd78bf6f5b2ec0ebed
pishon <kuczynskilukasz@gmail.com>
2016-11-24 20:00:32 +0100

    area has polygon, can be created of polygon

--- src/main/java/pl/gihon/fdd/poi/importer/KmlAreasReader.java
+++ src/main/java/pl/gihon/fdd/poi/importer/KmlAreasReader.java
@@ -1,14 +1,18 @@
 package pl.gihon.fdd.poi.importer;
 
 import com.google.common.collect.Lists;
+import de.micromata.opengis.kml.v_2_2_0.Kml;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import pl.gihon.fdd.poi.model.Area;
+import pl.gihon.fdd.poi.model.Polygon;
 
 import java.io.InputStream;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by luk on 2016-11-24.
@@ -18,8 +22,16 @@ public class KmlAreasReader {
 
     private static final Logger logger = LoggerFactory.getLogger(KmlAreasReader.class);
 
+    @Autowired
+    private KmlPolygonImporter polygonImporter;
+
     public List<Area> read(InputStream kmlFile) {
         logger.debug("reading for stream {} ", kmlFile);
-        return Lists.newArrayList(new Area());
+
+        List<Polygon> polygons = polygonImporter.importPolygons(kmlFile);
+        List<Area> areas = polygons.stream().map( p -> new Area(p)).collect(Collectors.toList());
+
+        return areas;
     }
+
 }
--- src/test/java/pl/gihon/fdd/poi/importer/KmlAreasReaderContextTest.java
+++ src/test/java/pl/gihon/fdd/poi/importer/KmlAreasReaderContextTest.java
@@ -1,10 +1,15 @@
 package pl.gihon.fdd.poi.importer;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import pl.gihon.fdd.poi.model.Area;
+
+import java.io.InputStream;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -21,6 +26,13 @@ public class KmlAreasReaderContextTest {
 
     @Test
     public void testRead() throws Exception {
-        reader.read(null);
+
+        InputStream is = this.getClass().getClassLoader().getResourceAsStream("poi test.kml");
+
+        List<Area> areas = reader.read(is);
+        assertThat(areas).hasSize(2);
+        Area area1st = areas.get(0);
+        assertThat(area1st.getName()).isEqualTo("zwierzyniec");
+        assertThat(area1st.hasPolygon()).isTrue();
     }
 }
